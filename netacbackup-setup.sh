8f48ccdfd582e8df09aad40ed4869512891c12b1
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-02-06 03:22:21 -0600

    Update netacbackup-setup.sh

--- netacbackup-setup.sh
+++ netacbackup-setup.sh
@@ -36,7 +36,7 @@ done
 
 ####SERVER.
 #: Dependancies.
-aDEPENDS=("gpg" "sudo" "rsync" "sshfs" "nginx" "ufw" "git")
+aDEPENDS=("gpg" "sudo" "rsync" "sshfs" "nginx" "certbot" "ufw" "git")
     #: Dependancy Check
 echo -e 'You will need the dependancies: '"${aDEPENDS[*]}"
 	while IFS= read -r -p $'If they are not installed, they will be now. Continue? (y/n)\n\n' sPLATFORM; do
