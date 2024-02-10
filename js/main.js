f41383feea268036aef9cc3f4533262a0f49b79b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-02-10 14:52:28 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ function setFormMessage(formElement, type, message) {
     messageElement.classList.add('form__message--${type}');
 }
 
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
 
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
@@ -15,5 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
         setFormMessage(loginForm, "error", "Invalid Username/Password");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("input", e => {
+            setFormMessage(loginForm, "error", "");
+        });
     });
 });
