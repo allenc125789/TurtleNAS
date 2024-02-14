4ccbdc8b66db57d24a645a34b8d63d24602d9e31
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-02-13 18:26:22 -0600

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -1,5 +1,1 @@
-if(!isset($_POST["submit"]))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
+<?php echo '<p>Hello World</p>';
