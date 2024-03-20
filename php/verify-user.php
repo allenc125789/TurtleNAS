e673e964b034fa5fd7950eb5310ae4e8d10f87d2
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-20 14:31:11 -0500

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -2,7 +2,7 @@
 
 $_POST['pword'];
 $password = $_POST['pword'];
-$output = shell_exec("bash /var/www/netacbackup/bash/hash-debian12.sh $password 2>&1");
+$output = shell_exec("bash /var/www/netacbackup/bash/pam-auth.sh $password 2>&1");
 echo "<pre>$output</pre>";
 
 //$output2 = shell_exec("whoami 2>&1");
