da95abfb7eb4fce620c3b95ba22dfa803f25143d
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-20 22:00:39 -0500

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -1,7 +1,7 @@
 <?php
 
 $password = $_POST['pword'];
-username = $_POST['uname'];
+$username = $_POST['uname'];
 $output = shell_exec("bash /var/www/netacbackup/bash/pam-auth.sh $password $username 2>&1");
 echo "<pre>$output</pre>";
 
