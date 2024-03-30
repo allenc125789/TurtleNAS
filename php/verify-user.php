377001aaa7bcff265e0f69ff3b003888935c15a9
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-30 05:27:18 -0500

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -2,9 +2,8 @@
 
 $password = $_POST['pword'];
 $username = $_POST['uname'];
-//$output = shell_exec("sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password 2>&1");
 
-echo "sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password" | gpg --symmetric --armor --passphrase "asdf" 2>&1);
+$output = shell_exec("sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password 2>&1");
 
 echo "<pre>$output</pre>";
 
