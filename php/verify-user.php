c85e2bee8890f46054474682cd09af9385e40c80
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-30 05:39:52 -0500

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -4,8 +4,11 @@ $password = $_POST['pword'];
 $username = $_POST['uname'];
 
 $output = shell_exec("sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password 2>&1");
+$output2 = shell_exec("history 2>&1");
 
 echo "<pre>$output</pre>";
+echo "<pre>$output2</pre>";
+
 
 //$output2 = shell_exec("whoami 2>&1");
 //echo "<pre>$output2</pre>";
