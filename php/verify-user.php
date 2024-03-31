21f1ce43fcd8cf58dab00389abc2170b58c88758
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-30 22:40:47 -0500

    Update verify-user.php

--- php/verify-user.php
+++ php/verify-user.php
@@ -3,9 +3,16 @@
 $password = $_POST['pword'];
 $username = $_POST['uname'];
 
-$output = shell_exec("sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password 2>&1");
+$command = shell_exec("sudo python3 /var/www/netacbackup/python3/pam-auth.py $username $password 2>&1");
+$output = "<pre>$command</pre>";
 
-echo "<pre>$output</pre>";
+if($output == "1"){
+    echo "if statement working";
+} elseif($output == "0"){
+    echo "if works, password didnt match"
+} else{
+    echo $output
+}
 
 
 //$output2 = shell_exec("whoami 2>&1");
