898a9e89b7d66d7b44823e4f8d4c33817795b1c6
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-01 04:24:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -77,7 +77,7 @@ chmod -R o=rx "$vPWD/turtlenas"
     #: Sudo.
 sudo adduser www-data sudo
 echo "www-data ALL=(ALL) !ALL" >> /etc/sudoers
-echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/python3 /var/www/turtlenas/python3/pam-auth.py*" >> /etc/sudoers
+echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/python3 ../python3/pam-auth.py*" >> /etc/sudoers
 sudo adduser sysadmin sudo
 echo "sysadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     #: Sets sudo "timestamp_timeout=" to 0 in /etc/sudoers, so verification is requested everytime needed.
