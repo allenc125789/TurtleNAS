7ffe2f1ca1b37e13c6fd652e1eb7d96c206e841d
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-03 03:12:34 -0500

    Update handle-auth-pam.php

--- turtlenas/private/php/handle-auth-pam.php
+++ turtlenas/private/php/handle-auth-pam.php
@@ -1,1 +1,12 @@
-su sysadmin -c " python3 /var/www/turtlenas/private/python3/pam-auth.py $1 $2"
+$command = shell_exec(" bash ../private/bash/handle-pam-auth.sh $username $password' 2>&1");
+$output = "$command";
+
+// Deny/Allow Access.
+if($output){
+    echo "if statement working";
+} elseif(!$output){
+    header('Location: /index.html');
+    exit;
+} else{
+    echo "<pre>$output</pre>";
+}
