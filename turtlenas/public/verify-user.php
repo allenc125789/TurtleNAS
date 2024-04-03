4259940aba167eb5cb6a731f003216cf6f61b25b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-03 03:09:02 -0500

    Update verify-user.php

--- turtlenas/public/verify-user.php
+++ turtlenas/public/verify-user.php
@@ -4,18 +4,7 @@ $password = $_POST['pword'];
 $username = $_POST['uname'];
 
 // Handle for verification.
-$command = shell_exec(" bash ../private/bash/handle-pam-auth.sh $username $password' 2>&1");
-$output = "$command";
 
-// Deny/Allow Access.
-if($output){
-    echo "if statement working";
-} elseif(!$output){
-    header('Location: /index.html');
-    exit;
-} else{
-    echo "<pre>$output</pre>";
-}
 
 
 ?>
