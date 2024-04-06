377210319ea29abc4659e44cece9276a8565a7da
root <root@home-ok-na01p.rezient.com>
2024-04-06 04:48:40 -0500

    rollback

---
+++ turtlenas/private/php/handle-pam-auth.php
@@ -0,0 +1,22 @@
+<?php
+
+
+if(empty($password || $username)){
+//    header('Location: /index.html');
+    exit;
+}
+
+$command = shell_exec("sudo python3 ../private/python3/pam-auth.py $username $password 2>&1");
+$output = "$command";
+
+if($output){
+    echo "if statement working";
+} elseif(!$output){
+    header('Location: /index.html');
+    exit;
+} else{
+    echo "<pre>$output</pre>";
+}
+
+
+?>
