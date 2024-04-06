6b761648bc705f1614bf1f6b85265bffa48e2880
root <root@home-ok-na01p.rezient.com>
2024-04-06 04:47:25 -0500

    rollback

--- turtlenas/public/verify-user.php
+++ turtlenas/public/verify-user.php
@@ -3,17 +3,6 @@
 $password = $_POST['pword'];
 $username = $_POST['uname'];
 
-$command = shell_exec("sudo python3 ../private/python3/pam-auth.py $username $password 2>&1");
-$output = "$command";
-
-if($output){
-    echo "if statement working";
-} elseif(!$output){
-    header('Location: /index.html');
-    exit;
-} else{
-    echo "<pre>$output</pre>";
-}
-
+include "../private/php/handle-pam-auth.php";
 
 ?>
