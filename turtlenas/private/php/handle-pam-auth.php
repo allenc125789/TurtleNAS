0a5ad8f17d606b70d5127e1998024a64def5c032
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-07 02:36:52 -0500

    Update handle-pam-auth.php

--- turtlenas/private/php/handle-pam-auth.php
+++ turtlenas/private/php/handle-pam-auth.php
@@ -1,17 +1,20 @@
 <?php
 
+// Restrict user
+$Restricted = array("root", "sysadmin");
+
 // Case/Authorize empty strings
 if(empty($password || $username)){
     header('Location: /index.html');
     exit;
-// Restrict user
-} elseif ($username == "root" || "sysadmin"){
+} elseif(in_array($username, $Restricted)){
     header('Location: /index.html');
     exit;
 } else {
     $command = shell_exec(" sudo python3 ../private/python3/pam-auth.py $username $password 2>&1");
     $output = "$command";
 }
+
 
 // Redirect
 
