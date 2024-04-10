98a3a40b19e8c6abc9f6eef4a65c2e65526d6751
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-10 18:52:18 -0500

    Update download.php

--- turtlenas/public/download.php
+++ turtlenas/public/download.php
@@ -1,5 +1,28 @@
 <?php
 
-echo $_SERVER['QUERY_STRING']
+session_start();
+
+$validated = $_SESSION['allowed'];
+switch ($validated) {
+    case 1:
+        echo "Downloading...";
+        break;
+    default:
+        header('Location: /index.html');
+        break;
+}
+$file = $_SERVER['QUERY_STRING'];
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename=' . basename($file));
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Pragma: public');
+header('Content-Length: ' . filesize($file));
+ob_clean();
+flush();
+readfile($file);
+exit;
 
 ?>
