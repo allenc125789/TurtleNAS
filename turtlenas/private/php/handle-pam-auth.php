ed7115a37ca5ffea298fd64eb7753f14450101a1
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-09 19:24:23 -0500

    Update handle-pam-auth.php

--- turtlenas/private/php/handle-pam-auth.php
+++ turtlenas/private/php/handle-pam-auth.php
@@ -28,7 +28,6 @@ if(empty($password || $username)){
 // Successful Auth.
 if($output){
     $_SESSION['allowed'] = 1;
-    header("Location: /backup-list.php");
 // Failed Auth.
 } elseif(!$output){
     header('Location: /index.html');
