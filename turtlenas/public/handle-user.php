eb697c9118d81e8893001773b7f60605d9df8079
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-09 19:14:56 -0500

    Create handle-user.php

---
+++ turtlenas/public/handle-user.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+$id = session_id();
+
+$command = shell_exec("cat /var/lib/php/sessions/sess_$id 2>&1");
+$output = "$command";
+
+// Verify Session
+switch (true) {
+    case strstr($output, 'allowed|i:1'):
+        echo "<pre>$output</pre>";
+        break;
+    default:
+        echo "not a valid session";
+        break;
+}
+
+?>
