ebe7aa5a2aed2f3d73ba8a701ebe948572bbaeb1
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-11 23:44:06 -0500

    Update admin-check.sh

--- turtlenas/private/bash/admin-check.sh
+++ turtlenas/private/bash/admin-check.sh
@@ -2,11 +2,14 @@
 
 # Used to check admin group privlige.
 
+
+# Parsing.
 string=$(getent group admin)
 rules=$(echo "$string" | sed "s/admin:.*://g")
 spaces=$(echo "$rules" | sed "s/,/ /g")
 admins=( $spaces )
 
+# Compare to Admin Group
 if [[ ${admins[@]} =~ $1 ]]
 then
     echo "1"
