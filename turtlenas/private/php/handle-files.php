3ce8b88abc97eea87cfb13c1b0d1c8425593956c
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-11 23:55:40 -0500

    Update handle-files.php

--- turtlenas/private/php/handle-files.php
+++ turtlenas/private/php/handle-files.php
@@ -20,6 +20,7 @@ function scanDirAndSubdir($dir, &$out = []) {
 // Directory to fetch. (edit path with variables "/media/$vLOCATION/$vDRIVE/$vUSER" for better reference by databases)
 $afiles = (scanDirAndSubdir("/media/Local/local/$username"));
 
+// List files in a browser format.
 foreach ($afiles as $a2) {
     echo "<a href='/download.php?$a2'>$a2</a><br>";
 }
