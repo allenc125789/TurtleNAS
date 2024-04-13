7b58bdcbf41c1795b948dd6dc506d3274a53a86f
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-13 18:36:02 -0500

    Update handle-downloads.php

--- turtlenas/private/php/handle-downloads.php
+++ turtlenas/private/php/handle-downloads.php
@@ -6,7 +6,18 @@ session_start();
 $validated = $_SESSION['allowed'];
 switch ($validated) {
     case 1:
-        echo "Downloading...";
+        $file = $_SERVER['QUERY_STRING'];
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename=' . basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
         break;
     default:
         header('Location: /index.html');
@@ -15,17 +26,5 @@ switch ($validated) {
 
 
 // Download file from query string.
-$file = $_SERVER['QUERY_STRING'];
-header('Content-Description: File Transfer');
-header('Content-Disposition: attachment; filename=' . basename($file));
-header('Content-Transfer-Encoding: binary');
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Pragma: public');
-header('Content-Length: ' . filesize($file));
-ob_clean();
-flush();
-readfile($file);
-exit;
 
 ?>
