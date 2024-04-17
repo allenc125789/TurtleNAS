a7829a38246cd17aca8659067344ec6878329988
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-17 06:47:07 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -3,7 +3,12 @@ include "../private/php/DBcontrol.php"
 
 // Authentication.
 if(validate_auth()) {
-    include "../private/php/handle-files.php";
+    // Directory to fetch. (edit path with variables "/media/$vLOCATION/$vDRIVE/$vUSER" for better reference by databases)
+    $afiles = (scanDirAndSubdir("/media/Local/local/$username"));
+// List files in a browser format.
+    foreach ($afiles as $a2) {
+        echo "<a href='/download.php?$a2'>$a2</a><br>";
+    }
 }
 
 ?>
