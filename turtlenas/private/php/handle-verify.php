4abfa3e20171144dde2edf2e9344df46e169b7e6
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-17 02:01:59 -0500

    Update handle-verify.php

--- turtlenas/private/php/handle-verify.php
+++ turtlenas/private/php/handle-verify.php
@@ -1,32 +1,30 @@
 <?php
 
 // Verify Session.
-function validate_session(){
+function validate_session() {
     $validated = $_SESSION['allowed'];
     switch ($validated) {
         case 1:
-            $a = True;
+            return true;
             break;
         default:
             header('Location: /login.html');
             break;
     }
-    return $a;
 }
 
 
 // Verify Privlige.
-function validate_privlige(){
+function validate_privlige() {
     $privlige = $_SESSION['admin'];
     switch ($privlige) {
         case 1:
-            $a = True;
+            return true;
             break;
         default:
             header('Location: /login.html');
             break;
     }
-    return $a;
 }
 
 ?>
