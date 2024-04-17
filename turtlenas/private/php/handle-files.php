6fb022f58079177816571d4e5c35cb8ff157edd9
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-17 05:19:20 -0500

    Update handle-files.php

--- turtlenas/private/php/handle-files.php
+++ turtlenas/private/php/handle-files.php
@@ -1,24 +1,5 @@
 <?php
 
-// Function to fetch file list from directory.
-function scanDirAndSubdir($dir, &$out = []) {
-    $sun = scandir($dir);
-
-    foreach ($sun as $a => $filename) {
-        $way = realpath($dir . DIRECTORY_SEPARATOR . $filename);
-// List Files.
-        if (!is_dir($way)) {
-            $out[] = $way;
-// List Directories.
-        } else if ($filename != "." && $filename != "..") {
-            scanDirAndSubdir($way, $out);
-            $out[] = ("$way/");
-        }
-    }
-
-    return $out;
-}
-
 // Directory to fetch. (edit path with variables "/media/$vLOCATION/$vDRIVE/$vUSER" for better reference by databases)
 $afiles = (scanDirAndSubdir("/media/Local/local/$username"));
 
