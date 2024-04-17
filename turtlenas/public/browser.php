6ebe16e3c08dff16918385765f09f49eba4699a4
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-17 02:17:28 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -5,7 +5,7 @@ include "../private/php/handle-pam-auth.php";
 
 // Verify.
 include "../private/php/handle-verify.php";
-if(validate_session()) {
+if(validate_auth()) {
     include "../private/php/handle-files.php";
 }
 
