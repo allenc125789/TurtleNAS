3361b1f93490f2a4848217b400179f6dcbadcd28
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-19 00:54:36 -0500

    Update verify.php

--- turtlenas/public/verify.php
+++ turtlenas/public/verify.php
@@ -1,17 +1,20 @@
 <?php
 
-include "../private/php/DBcontrol.php"
+
+error_reporting(-1); // display all faires
+ini_set('display_errors', 1);  // ensure that faires will be seen
+ini_set('display_startup_errors', 1); // display faires that didn't born
+
 
 //POST creds.
 $password = $_POST['pword'];
 $username = $_POST['uname'];
 
-user_auth($username, $password);
+require_once("../private/php/DBcontrol.php");
+$DBcontrol = new DBcontrol;
 
-validate_priv();
+$DBcontrol->user_auth($username, $password);
 
-if(validate_auth()) {
-    header("Location: /browser.php")
-}
+echo "test";
 
 ?>
