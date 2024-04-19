85849e944b94c225ef3a715aca17702692c7c35b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-19 01:22:25 -0500

    Update verify.php

--- turtlenas/public/verify.php
+++ turtlenas/public/verify.php
@@ -1,17 +1,13 @@
 <?php
 
-
-error_reporting(-1); // display all faires
-ini_set('display_errors', 1);  // ensure that faires will be seen
-ini_set('display_startup_errors', 1); // display faires that didn't born
+// Call DBcontrol and declare it's class.
+require_once("../private/php/DBcontrol.php");
+$DBcontrol = new DBcontrol;
 
 
 //POST creds.
 $password = $_POST['pword'];
 $username = $_POST['uname'];
-
-require_once("../private/php/DBcontrol.php");
-$DBcontrol = new DBcontrol;
 
 $DBcontrol->user_auth($username, $password);
 
