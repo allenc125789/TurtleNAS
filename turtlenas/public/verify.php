6d62b61b3a2e21c2628ad9310248f831e377a509
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-19 02:35:02 -0500

    Update verify.php

--- turtlenas/public/verify.php
+++ turtlenas/public/verify.php
@@ -5,11 +5,9 @@ $control = new DBcontrol;
 //POST creds.
 $password = $_POST['pword'];
 $username = $_POST['uname'];
+echo "<a href='/login.html'>Verifying...</a>";
 
 $control->user_auth($username, $password);
-$redirect = $control->redirect_login();
-return  <a href="$redirect">Verifying...</a>;
-
 $verify = $control->validate_auth();
 if($verify){
     echo "test";
