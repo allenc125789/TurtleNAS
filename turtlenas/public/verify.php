9d8bcf25fbc8e3eba69b1e0e046f93519d0b77a6
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-04-20 01:47:53 -0500

    Update verify.php

--- turtlenas/public/verify.php
+++ turtlenas/public/verify.php
@@ -1,13 +1,16 @@
 <?php
+
+// Call DBcontrol and declare it's class.
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
 //POST creds.
 $password = $_POST['pword'];
 $username = $_POST['uname'];
-echo "<a href='/login.html'>Verifying...</a>";
 
 $control->user_auth($username, $password);
+echo "<a href='/login.html'>Verifying...</a>";
+
 $verify = $control->validate_auth();
 if($verify){
     echo "test";
