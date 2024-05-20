a7f8126ba80bf1641bbb1e54d39997eadc96593c
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-20 10:34:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,11 +88,8 @@ mariadb -e "CREATE DATABASE turtlenas;"
     #: Create table for Mapped Drive Locations.
 mariadb -e "USE turtlenas; CREATE TABLE drives (user VARCHAR(50) PRIMARY KEY, type VARCHAR(6), disk VARCHAR(10), uuid CHAR(36) );"
 mariadb -e "USE turtlenas; INSERT INTO drives (user, type, disk, uuid) VALUES('admin', 'LOCAL', '$vFILESYSTEM', '$vUUID');"
-    #: Create table for the admin user.
-mariadb -e "USE turtlenas; CREATE TABLE files_admin (dir VARCHAR(100) PRIMARY KEY, file VARCHAR(100) );"
-    #: Create table for a Command Qeue to be executed by the sysadmin user.
-mariadb -e "USE turtlenas; CREATE TABLE command_qeue (user VARCHAR(50) PRIMARY KEY, command VARCHAR(6), auth INT );"
-
+mariadb -t -e "CREATE USER 'www-data'@'localhost'"
+mariadb -e "GRANT ALL PRIVILEGES ON turtlenas.drives TO 'www-data'@'localhost' WITH GRANT OPTION"
 
 #: Web Server Configuration.
 echo -e "Configuring web server..."
