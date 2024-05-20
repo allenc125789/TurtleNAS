c74aabe68069a67687237ff8aa3ff37d87654f2c
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-20 13:26:26 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -9,9 +9,9 @@ if($verify){
     $afiles = $control->scanDirAndSubdir("/media/LOCAL/5d2aee01-0ecd-4ac4-b2ca-796b24be7e34/admin");
     // List files in a browser format.
     foreach ($afiles as $a2) {
-        echo "<a href='/download.php?$a2'>$a2</a><br>";
+        //echo "<a href='/download.php?$a2'>$a2</a><br>";
+        $control->getPathBySQL();
     }
-//    echo "true";
 }
 
 
