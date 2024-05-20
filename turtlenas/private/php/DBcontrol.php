614b10a0a19bfc4f166464d569fdc9c1f58d66dc
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-20 00:40:47 -0500

    Update DBcontrol.php

--- turtlenas/private/php/DBcontrol.php
+++ turtlenas/private/php/DBcontrol.php
@@ -94,5 +94,15 @@ class DBcontrol {
         return $out;
     }
 
+    public function scanDirAndSubdir() {
+        if($verify){
+            // Directory to fetch. (edit path with variables "/media/$vLOCATION/$vDRIVE/$vUSER" for better reference by databases)
+            $afiles = $this->scanDirAndSubdir("/media/LOCAL/5d2aee01-0ecd-4ac4-b2ca-796b24be7e34/admin");
+            // List files in a browser format.
+            foreach ($afiles as $a2) {
+                echo "<a href='/download.php?$a2'>$a2</a><br>";
+        }
+    }
+
 }
 ?>
