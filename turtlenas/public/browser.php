80183eacdcb93fc58efc5e4d95edcc7ef55adb0b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-20 15:12:48 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -11,18 +11,13 @@ ini_set('display_startup_errors', 1); // display faires that didn't born
 $verify = $control->validate_auth();
 if($verify){
     // Directory to fetch. (edit path with variables "/media/$vLOCATION/$vDRIVE/$vUSER" for better reference by databases)
-    $afiles = $control->scanDirAndSubdir("/media/LOCAL/5d2aee01-0ecd-4ac4-b2ca-796b24be7e34/admin");
+    $path = $control->getPathByUser();
+    $afiles = $control->scanDirAndSubdir($path);
     // List files in a browser format.
-//    foreach ($afiles as $a2) {
-        //echo "<a href='/download.php?$a2'>$a2</a><br>";
-//    }
-    $control->getPathByUser();
+    foreach ($afiles as $a2) {
+        echo "<a href='/download.php?$a2'>$a2</a><br>";
+    }
 }
 
-
-
-
-// Authentication.
-//if($valuser) {
 
 ?>
