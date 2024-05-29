e3bf0a34f476eedcce8849a74b29bc6c7241bb0e
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-28 20:42:40 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -10,7 +10,7 @@ $verify = $control->validate_auth();
 if($verify){
     $control->updateFileRecord();
     $fObject = $control->getFilesForDisplay();
-//    $control->switchWorkingDir();
+    $query = $control->getParentByQuery();
 }
 
 ?>
@@ -25,12 +25,15 @@ if($verify){
         </tr>
         <?php foreach($fObject as $row):?>
         <?php $data = explode('|', $row);?>
+        <?php if (!is_null($query)):?>
+            <td><?php echo "<a href='/browser.php?$query'>../";?></td>
+        <?php endif;?>
         <tr>
             <?php if (is_dir($data[0])):?>
                 <td><?php echo "<a href='/browser.php?/$data[1]'>$data[1]";?></td>
-            <?php else :?>
+            <?php else:?>
                 <td><?php echo "<a href='/download.php?$data[1]'>$data[1]";?></td>
-            <?php endif; ?>
+            <?php endif;?>
             <td><?php echo $data[1];?></td>
             <td><?php echo $data[2];?></td>
             <?php endforeach;?>
