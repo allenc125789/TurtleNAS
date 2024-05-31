e2995baf2a1d48c5b5c6b24de23ef534cc6d35ae
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-31 12:07:44 -0500

    Update upload.php

--- turtlenas/public/upload.php
+++ turtlenas/public/upload.php
@@ -4,33 +4,37 @@ $control = new DBcontrol;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-$temp_file = tempnam(sys_get_temp_dir(), 'Tux');
 
-echo $temp_file;
+function pre_r( $array ){
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
 
+function reArrayFiles(&$file_post) {
+    $file_ary = array();
+    $file_count = count($file_post['name']);
+    $file_keys = array_keys($file_post);
+    for ($i=0; $i<$file_count; $i++) {
+        foreach ($file_keys as $key) {
+            $file_ary[$i][$key] = $file_post[$key][$i];
+        }
+    }
+    return $file_ary;
+}
 
 $query = $_SERVER['QUERY_STRING'];
 $username = $_SESSION['sessuser'];
 $path = str_replace("$username:/", '', $query);
-$file = $control->getFullPath($path);
-
-if (isset($_POST['submit'])){
-    $fileName = $_FILES['file']['name'];
-    $fileTmpName = $_FILES['file']['tmp_name'];
-    $fileSize = $_FILES['file']['size'];
-    $fileError = $_FILES['file']['error'];
-    $fileType = $_FILES['file']['type'];
-
+$fullpath = $control->getFullPath($path);
+if (isset($_FILES['file'])){
+    $file_array = reArrayFiles($_FILES['file']);
+    for ($i=0;$i<count($file_array);$i++){
+        move_uploaded_file($file_array[$i]['tmp_name'], $fullpath . $file_array[$i]['name']);
+        echo $fullpath . $file_array[$i]['name'];
+    }
 }
-//if ($filesize < 500000){
-    $fileDestination = $file . $fileName;
-    move_uploaded_file($fileTmpName, $fileDestination);
-//    header("Location: /browser.php?$query");
-    echo $fileDestination . $fileTmpName;
-//} else {
-    echo "File size too large!";
-//}
-
+header("Location: /browser.php?$query");
 
 
 
