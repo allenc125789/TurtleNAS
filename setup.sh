27f303757e58bb6389b587c8cef9477938077c1d
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-05-31 19:27:08 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ sed -i "s/@/$vDOMAIN/g" $vPWD"/turtlenas-config"
 mv "$vPWD/extra/turtlenas-config" "/etc/nginx/sites-available/turtlenas-config"
 mv -f "$vPWD/extra/nginx.conf" "/etc/nginx/nginx.conf"
 mv "$vPWD/extra/User-Manual.txt" "/media/LOCAL/$vUUID/admin"
-mv "$vPWD/extra/php.ini" "/etc/php/8.2/fpm"
+mv -f "$vPWD/extra/php.ini" "/etc/php/8.2/fpm"
 rm -f /etc/nginx/sites-enabled/default
 ln -v -s /etc/nginx/sites-available/turtlenas-config /etc/nginx/sites-enabled/
     #: Web page files.
