fefa6e34cfdbf7777f354e3d934152890ae794db
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-06-06 07:58:05 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -26,7 +26,7 @@ if($casequery || $query == NULL || $username == NULL){
 <html>
 <head>
     <meta charset="UTF=8">
-    <meta name="viewport" content="width==device-width, initial-scale=0.4">
+    <meta name="viewport" content="width==device-width, initial-scale=0.1">
     <link rel="stylesheet" href="/css/browser.css">
 </head>
 
