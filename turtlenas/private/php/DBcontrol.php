c173b35bc58663f2958b65490e522f6d46669f20
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-06-09 08:50:58 -0500

    Update DBcontrol.php

--- turtlenas/private/php/DBcontrol.php
+++ turtlenas/private/php/DBcontrol.php
@@ -1,359 +1,93 @@
 <?php
-session_start();
+include "../private/php/DBcontrol.php";
+$control = new DBcontrol;
 
-class DBcontrol {
-    private $host;
-    private $dbname;
-    private $user;
-    private $pass;
-    private $charset;
+error_reporting(-1); // display all faires
+ini_set('display_errors', 1);  // ensure that faires will be seen
+ini_set('display_startup_errors', 1); // display faires that didn't born
 
-    public function redirect_login(){
-        header('Location: /login.html');
-    }
-
-    public function get_connection(){
-        $this->host = "localhost";
-        $this->dbname = "turtlenas";
-        $this->user = "www-data";
-        $this->pass = "";
-        $this->charset = "utf8mb4";
-        try{
-            $dsn = "mysql:host=".$this->host.";dbname=".$this->dbname.";charset=".$this->charset;
-            $pdo = new PDO($dsn, $this->user, $this->pass);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        } catch (PDOException $e){
-            echo "Connection failed: ".$e->getMessage();
-        }
-    }
-
-    public function prepFileSize($fullpath, $size='0', $unit=''){
-        if (is_dir($fullpath)){
-            $scandir = $this->scanDirAndSubdir($fullpath, $_FilesOnly = TRUE);
-            foreach ($scandir as $files){
-                $sfiles = filesize($files);
-                $size += $sfiles;
-            }
-        } else{
-            $size = filesize($fullpath);
-        }
-        if( (!$unit && $size >= 1<<30) || $unit == "GB"){
-            return number_format($size/(1<<30),2)."GB";
-        } if( (!$unit && $size >= 1<<20) || $unit == "MB"){
-            return number_format($size/(1<<20),2)."MB";
-        } if( (!$unit && $size >= 1<<10) || $unit == "KB"){
-            return number_format($size/(1<<10),2)."KB";
-        }
-        return number_format($size)."B";
-    }
-
-    public function prepFileDate($fullpath){
-        if (file_exists($fullpath)) {
-            return date("m/d/y (H:i:s)", filemtime($fullpath));
-        }
-    }
-
-    public function prepFileHash($fullpath, $hfile = ''){
-        if ((file_exists($fullpath)) && (!is_dir($fullpath))) {
-            return hash_file('sha224', "$fullpath");
-        } elseif (file_exists($fullpath)) {
-            $scandir = $this->scanDirAndSubdir($fullpath, $_FilesOnly = TRUE);
-            foreach ($scandir as $file){
-                $hfile .= hash_file('sha224', "$file");
-            }
-            return hash('sha224', "$hfile");
-        }
-    }
-
-    public function getDiskByUser(){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->query("SELECT * FROM drives WHERE user = '$username'");
-        while ($row = $stmt->fetch()){
-            $allrows = $row['type']. "|".$row['uuid']. "|".$row['disk']. "|".$row['user'];
-            $data[] = $allrows;
-        }
-        return $username;
-    }
-
-    public function getShortPath($fullpath){
-        $username = $_SESSION['sessuser'];
-        $root = $this->getRootByUser();
-        $parent = str_replace($root,'',$fullpath);
-        if ("$fullpath/" == $root){
-            $parent = '/';
-            return $parent;
-        } else {
-            return "/$parent/";
-        }
-    }
-
-    public function getFullPath($shortpath){
-        $root = $this->getRootByUser();
-        $filename = ($root . $shortpath);
-        $fullpath = str_replace("%20", " ", $filename);
-        return $fullpath;
-    }
-
-    public function getFullPathByHash($hash){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->query("SELECT fullpath FROM files_$username WHERE hash = '$hash'");
-        while ($row = $stmt->fetch()){
-            $fullpath = $row['fullpath'];
-            return $fullpath;
-        }
-    }
-
-    public function getFilesForDisplay(){
-        $username = $_SESSION['sessuser'];
-        $query = $_SERVER['QUERY_STRING'];
-        $query = str_replace("%20", " ", $query);
-        $path = str_replace("$username:", '', "$query");
-        $stmt = $this->get_connection()->query("SELECT * FROM files_$username WHERE parent = '$path'");
-        while ($row = $stmt->fetch()){
-            $allrows = $row['fullpath']. "|".$row['name']. "|".$row['date']. "|".$row['size']. "|".$row['parent']. "|".$row['hash'];
-            $data[] = $allrows;
-        }
-        return $data;
-    }
-
-    public function getRootByUser(){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->query("SELECT * FROM drives WHERE user = '$username'");
-        while ($row = $stmt->fetch()){
-            $root = "/media/".$row['type']. "/".$row['uuid']. "/".$row['user']. "/";
-            return $root;
-        }
-    }
-
-    public function getParentByQuery(){
-        $username = $_SESSION['sessuser'];
-        $query = $_SERVER['QUERY_STRING'];
-        $query = str_replace("%20", " ", $query);
-        $path = str_replace("$username:", '', "$query");
-        $root = $this->getRootByUser();
-        if ($path !== "/"){
-            $path = ltrim($path, '/');
-            $path = $this->getFullPath($path);
-            $path = dirname($path);
-            $path = $this->getShortPath($path);
-            return $path;
-        }
-        $stmt = $this->get_connection()->query("SELECT parent FROM files_$username WHERE name = '$path'");
-        while ($row = $stmt->fetch()){
-            $parent = $row['parent'];
-            return $parent;
-
-        }
-    }
-
-    public function getPathByPath($data = ''){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->query("SELECT fullpath FROM files_$username");
-        while ($row = $stmt->fetch()){
-            $newpath = $row['fullpath'];
-            $data .= ("$newpath|");
-            $sqlpaths = explode('|', $data);
-        }
-        return $sqlpaths;
-    }
-
-    public function getHashByPath($fullpath, $data = '', $sqlhash = ''){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->query("SELECT hash FROM files_$username WHERE fullpath = '$fullpath'");
-        while ($row = $stmt->fetch()){
-            $sqlhash = $row['hash'];
-        }
-        return $sqlhash;
-    }
-
-    public function deleteFile(){
-        $post = $_POST['fileToDelete'];
-        $username = $_SESSION['sessuser'];
-        $query = $_SERVER['QUERY_STRING'];
-        $parent = str_replace("$username:/", '', "$query");
-        $root = $this->getRootByUser();
-        foreach ($post as $filename){
-            $filename = $root . $parent . $filename;
-            $filename = str_replace("%20", " ", $filename);
-            if (is_dir($filename)){
-                $arr = $this->scanDirAndSubdir($filename);
-                foreach ($arr as $file){
-                    unlink($file);
-                    rmdir($file);
-                }
-                rmdir($filename);
-            } else{
-                unlink($filename);
-                echo $filename;
-            }
-        }
-        $_POST = array();
-        header("Location: /browser.php?$query");
-    }
-
-    public function createDir($post){
-        $username = $_SESSION['sessuser'];
-        $query = $_SERVER['QUERY_STRING'];
-        $query = str_replace("%20", " ", $query);
-        $parent = str_replace("$username:/", '', "$query");
-        $root = $this->getRootByUser();
-        foreach ($post as $dir){
-            $pos = strpos($dir, $query);
-            if ($pos === false && str_contains($dir, $root)){
-                $newdir = str_replace("$root$parent", '', $dir);
-                mkdir($root . $parent . $newdir, 0777, true);
-//                echo "$root$parent$newdir ";
-            } elseif (!str_contains($dir, $root)){
-                mkdir($root . $parent . $dir);
-//                echo $dir;
-//        header("Location: /browser.php?$query");
-            }
-        }
-    }
-
-    public function deleteRecordByPath($vfullpath){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->prepare("DELETE FROM files_$username WHERE fullpath = :vfullpath");
-        $stmt->bindParam(':vfullpath', $vfullpath, PDO::PARAM_STR);
-        $stmt->execute(['vfullpath' => $vfullpath]);
-    }
-
-    public function getInsertFileRecord($vfullpath, $vparent, $vname, $vdate, $vsize, $vhash){
-        $username = $_SESSION['sessuser'];
-        $stmt = $this->get_connection()->prepare("INSERT INTO files_$username (fullpath, parent, name, date, size, hash) VALUES (:vfullpath, :vparent, :vname, :vdate, :vsize, :vhash)");
-        $stmt->execute([
-            'vfullpath' => addslashes($vfullpath),
-            'vparent' => $vparent,
-            'vname' => $vname,
-            'vdate' => $vdate,
-            'vsize' => $vsize,
-            'vhash' => $vhash,
-        ]);
-    }
-
-//    public function save_to_file_manager($data=[]){
-
-    public function user_auth($username, $password) {
-        // Restricted users.
-        $restricted = array("root", "sysadmin");
-        // Deny empty strings.
-        if(empty($password || $username)){
-            $this->redirect_login();
-            exit;
-        // Deny restricted users.
-        } elseif(in_array($username, $restricted)){
-            $this->redirect_login();
-            exit;
-        // Allow all else.
-        } else {
-            $command = shell_exec(" sudo python3 ../private/python3/pam-auth.py $username $password 2>&1");
-            $output = "$command";
-        }
-        // Run Python3 code through Bash.
-        // Successful Auth.
-        if($output){
-            $_SESSION['allowed'] = 1;
-            $_SESSION['sessuser'] = $username;
-        // Failed Auth.
-        } elseif(!$output){
-            $this->redirect_login();
-            exit;
-        // Error (Add logs)
-        } else{
-            $this->redirect_login();
-            exit;
-        }
-        // Set Privlige through Bash.
-        $command2 = shell_exec(" bash ../private/bash/admin-check.sh $username 2>&1");
-        $output2 = "$command2";
-        // Admin True
-        if ($output2 == "1"){
-            $_SESSION['admin_status'] = 1;
-        // Admin False
-        } else{
-            $_SESSION['admin_status'] = 0;
-        }
-    }
-
-    // Verify Session.
-    public function validate_auth() {
-        $validated = $_SESSION['allowed'];
-        switch ($validated) {
-            case 1:
-                return true;
-                break;
-            default:
-                $this->redirect_login();
-                break;
-        }
-    }
-
-    // Verify Privlige.
-    public function validate_priv() {
-        $privlige = $_SESSION['admin_status'];
-        switch ($privlige) {
-            case 1:
-                return true;
-                break;
-            default:
-                $this->redirect_login();
-                break;
-        }
-    }
-
-    // Function to fetch file list from directory.
-    public function scanDirAndSubdir($dir, $_FilesOnly = FALSE, &$out = []) {
-        $username = $_SESSION['sessuser'];
-        $root = $this->getRootByUser();
-        $sun = scandir(($dir));
-        foreach ($sun as $a => $filename) {
-            $way = realpath($dir . DIRECTORY_SEPARATOR . $filename);
-    // List Files.
-            if (!is_dir($way)) {
-                $out[] = $way;
-    // List Directories.
-            } else if (!$_FilesOnly && $filename != "." && $filename != "..") {
-                $this->scanDirAndSubdir($way, FALSE, $out);
-                $out[] = ("$way/");
-            } else if ($_FilesOnly && $filename != "." && $filename != "..") {
-                $this->scanDirAndSubdir($way, TRUE, $out);
-            }
-        }
-        return $out;
-    }
-
-    public function updateFileRecord() {
-        $username = $_SESSION['sessuser'];
-        $root = $this->getRootByUser();
-        $afiles = $this->scanDirAndSubdir($root);
-        $sqlpathcheck = $this->getPathByPath();
-        // Remove old files from database.
-        foreach ($sqlpathcheck as $sqlpath) {
-            $sqlhashcheck = $this->getHashByPath($sqlpath);
-            $realhash = $this->prepFileHash($sqlpath);
-            if ((!file_exists($sqlpath)) || ($sqlhashcheck !== $realhash)) {
-                $this->deleteRecordByPath($sqlpath);
-            } else {
-                continue;
-            }
-        }
-        // Insert new files into database.
-        foreach ($afiles as $fullpath) {
-            $parse = dirname($fullpath). '/';
-            $parent = str_replace($root, '/', $parse);
-            $filename = str_replace($parse, '', $fullpath);
-            $date = $this->prepFileDate($fullpath);
-            $size = $this->prepFileSize($fullpath);
-            $hash = $this->prepFileHash($fullpath);
-            try {
-                $this->getInsertFileRecord($fullpath, $parent, $filename, $date, $size, $hash);
-            } catch (PDOException $e) {
-                continue;
-            }
-        }
-    }
-
+$verify = $control->validate_auth();
+if ($verify){
+    $control->updateFileRecord();
+    $fObject = $control->getFilesForDisplay();
+    $username = $_SESSION['sessuser'];
+    $query = $_SERVER['QUERY_STRING'];
+    $query = urldecode(str_replace("%20", " ", $query));
+    $casequery = str_starts_with("$username:", $query);
+    $queryparent = $control->getParentByQuery();
 }
 
+if ($casequery || $query == NULL || $username == NULL){
+    header("Location: /browser.php?$username:/");
+    header('Location: /login.html');
+}
 ?>
+
+<html>
+<head>
+    <meta charset="UTF=8">
+    <meta name="viewport" content="width==device-width, initial-scale=0.1">
+    <link rel="stylesheet" href="/css/browser.css">
+</head>
+
+
+<tbody>
+    <table class="fileTables" border=2px>
+        <tr bgcolor="grey">
+            <th colspan=2>File Name</th>
+            <th>Last Modified</th>
+            <th>File Size</th>
+        </tr>
+        <tr class="hotbar">
+            <td bgcolor="white"><input type="checkbox" name="massSelect[]" id="massSelect" onchange="checkAll(this)"/></td>
+            <td style="font-size: 20" bgcolor="white"><?php echo "<a href='/browser.php?$username:/'>⟲</a>";?>
+            <?php if (!is_null($queryparent)):?>
+            <?php echo "<a href='/browser.php?$username:$queryparent'>↩</a>";?></td>
+            <?php endif;?>
+            <td colspan=2 style="font-size:12" bgcolor="black"><font style="color:white;"><?php echo $query;?></font></td>
+        </tr>
+        <?php echo "<form action='/delete.php?$query' method='post'>";?>
+        <?php foreach($fObject as $row):?>
+        <?php $data = explode('|', $row);?>
+        <?php $arrkey = array_search($row, $fObject);?>
+        <tr class="tableItems" bgcolor="lightgrey">
+            <td id="checks"><?php echo "<input type=\"checkbox\" class=\"cb\" id=\"filechecks\" name=\"fileToDelete[]\" value=\"$data[1]\">";?></td>
+            <?php if (is_dir(stripslashes($data[0]))):?>
+            <td id="files"><?php $dir = (urlencode("$data[4]$data[1]")); echo "<a href='/browser.php?$username:$dir'>$data[1]";?></td>
+            <?php else:?>
+            <td id="files"><?php echo "<a href='/download.php?$username:$data[5]'>$data[1]";?></td>
+            <?php endif;?>
+            <td id="dates"><?php echo $data[2];?></td>
+            <td id="size"><?php echo $data[3];?></td>
+            <?php endforeach;?>
+            <?php if ($fObject == NULL):?>
+            <?php echo "<td></td><td>No files to display...</td><td></td><td></td>";?>
+            <?php endif;?>
+        </tr>
+    </table>
+</tbody>
+
+<div class="buttonDivs">
+<input type="submit" value="Delete" id="delete">
+<br><br>
+
+</form>
+
+    <?php echo "<form action='/upload.php?$query' method='POST' enctype='multipart/form-data'>"?>
+    <input type="file" id="button" name="file[]" multiple="" onchange="this.form.submit()">
+    </form>
+
+    <?php echo "<form action='/uploadDir.php?$query' method='POST' enctype='multipart/form-data'>"?>
+    <input type="file" id="filepicker" name="dir[]" onchange="this.form.submit()" webkitdirectory mozdirectory multiple />
+    </form>
+
+    <?php echo "<form action='/mkdir.php?$query' method='POST'>"?>
+    <button type="submit" id="mkdir">Create Folder...</button>
+    <input type="text" id="createDir" name="createDir" required minlength="1" maxlength="255" size="10" />
+    </form>
+</div>
+
+
+<script src="/js/browser.js"></script>
+
+</html>
