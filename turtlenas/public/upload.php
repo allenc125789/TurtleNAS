7fd808d3252d447905f27667d33e667ee733a7c7
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-06-11 07:18:00 -0500

    Update upload.php

--- turtlenas/public/upload.php
+++ turtlenas/public/upload.php
@@ -1,41 +1,10 @@
 <?php
-include "../private/php/DBcontrol.php";
+require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-
-function pre_r( $array ){
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
+$verify = $control->validate_auth();
+if($verify){
+    $control->uploadFile();
 }
-
-function reArrayFiles(&$file_post) {
-    $file_ary = array();
-    $file_count = count($file_post['name']);
-    $file_keys = array_keys($file_post);
-    for ($i=0; $i<$file_count; $i++) {
-        foreach ($file_keys as $key) {
-            $file_ary[$i][$key] = $file_post[$key][$i];
-        }
-    }
-    return $file_ary;
-}
-
-$query = $_SERVER['QUERY_STRING'];
-$query = str_replace("%20", " ", $query);
-$username = $_SESSION['sessuser'];
-$path = str_replace("$username:/", '', $query);
-$fullpath = $control->getFullPath($path);
-if (isset($_FILES['file'])){
-    $file_array = reArrayFiles($_FILES['file']);
-    for ($i=0;$i<count($file_array);$i++){
-        move_uploaded_file($file_array[$i]['tmp_name'], $fullpath . $file_array[$i]['name']);
-    }
-}
-
-header("Location: /browser.php?$query");
-
 
 ?>
