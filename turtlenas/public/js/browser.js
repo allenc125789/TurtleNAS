ed22f94a4fb338c5aee9c5a3cbb6476648dca0fc
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-06-23 11:12:10 -0500

    Update browser.js

--- turtlenas/public/js/browser.js
+++ turtlenas/public/js/browser.js
@@ -1,38 +1,26 @@
 document.getElementById('delete').disabled = true;
-var results = document.getElementsByClassName("cb");
-let count = 0;
-Array.prototype.forEach.call(results, function(checks) {
-    checks.addEventListener('change', function(e) {
-        if (checks.checked == true) {
-        count += 1;
-        } else {
-        count -= 1;
-        }
-        if (count == 0) {
-            document.getElementById('delete').disabled = true;
-        } else {
-            document.getElementById('delete').disabled = false;
-        }
-    });
-});
+
+//function deleteItems() {
+//    var results = document.getElementsByClassName("cb");
+//    var count = 0;
+//    Array.prototype.forEach.call(results, function(checks) {
+//        checks.addEventListener('change', function(e) {
+//            if (checks.checked == true) {
+//                count += 1;
+//            } else {
+//                count -= 1;
+//            }
+//            if (count == 0) {
+//                document.getElementById('delete').disabled = true;
+//            } else {
+//                document.getElementById('delete').disabled = false;
+//            }
+//        });
+//    });
+//}
 
 
 
-//var arr = [];
-//document.getElementById("filepicker").addEventListener(
-//  "change",
-//  (event) => {
-//    let output = document.getElementById("listing");
-//    for (const file of event.target.files) {
-//      let item = document.createElement("li");
-//      item.textContent = file.webkitRelativePath;
-//      console.log(file.webkitRelativePath);
-//    }
-//  },
-//  false,
-//);
-
-//console.log(arr);
 
 function checkAll(ele) {
     var checkboxes = document.getElementsByClassName('cb');
