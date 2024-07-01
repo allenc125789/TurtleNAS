4f4d675211edf31d6ac2ee637cad777d467010b4
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-06-30 23:40:27 -0500

    Update updateRecords.php

--- turtlenas/public/updateRecords.php
+++ turtlenas/public/updateRecords.php
@@ -3,9 +3,8 @@ include "../private/php/DBcontrol.php";
 $control = new DBcontrol;
 
 $verify = $control->validate_auth();
-if ($verify){
+if($verify){
     $control->updateFileRecord();
-//    sleep(5);
 }
 
 ?>
