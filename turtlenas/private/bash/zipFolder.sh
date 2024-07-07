92bc1a8c89fc79ef9a6f7fb9c32bad9be3b91389
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-07 04:58:31 -0500

    Create zipFolder.sh

---
+++ turtlenas/private/bash/zipFolder.sh
@@ -0,0 +1,11 @@
+# $1=ENCRYPT/PLAINTEXT, $2=CWD, $3=PASSWORD
+
+base_name=$(basename $2)
+hash=$(echo -n $base_name | sha224sum | sed 's/  .$//')
+if [[ $1 == "ENCRYPT" ]]; then
+    zip -e -r "/tmp/${hash}.zip" $2 -P $3
+elif [[ $1 == "PLAINTEXT" ]]; then
+    zip -r "/tmp/${hash}.zip" $2
+fi
+
+echo "/tmp/${hash}.zip"
