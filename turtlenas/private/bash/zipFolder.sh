1bf5c29e8093f2c6fe9636edd84a38028db73218
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-08 00:53:57 -0500

    Update zipFolder.sh

--- turtlenas/private/bash/zipFolder.sh
+++ turtlenas/private/bash/zipFolder.sh
@@ -3,11 +3,12 @@
 # $1=ENCRYPT/PLAINTEXT, $2=CWD, $3=PASSWORD
 
 base_name=$(basename $2)
-hash=$(echo -n $base_name | sha224sum | sed 's/  .$//')
-if [[ $1 == "ENCRYPT" ]]; then
-    zip -e -r "/tmp/${hash}.zip" $2 -P $3
-elif [[ $1 == "PLAINTEXT" ]]; then
-    zip -r "/tmp/${hash}.zip" $2
+hash=$(echo -n $base_name | sha224sum | sed 's/ .$//')
+if [ $1 == "ENCRYPT" ]; then
+    zip -q -e -r "/tmp/${hash}.zip" $2 -P $3
+    echo "${hash}.zip"
+elif [ $1 == "PLAINTEXT" ]; then
+    rm "/tmp/${hash}.zip"
+    zip -q -r -Z "deflate" -o "/tmp/${hash}.zip" $2
+    echo "${hash}.zip"
 fi
-
-echo "/tmp/${hash}.zip"
