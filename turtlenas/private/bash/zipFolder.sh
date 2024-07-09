7ef75b61298bef19237909cca5d2d5f092586244
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-09 00:29:44 -0500

    Update zipFolder.sh

--- turtlenas/private/bash/zipFolder.sh
+++ turtlenas/private/bash/zipFolder.sh
@@ -2,9 +2,9 @@
 
 # $1=ENCRYPT/PLAINTEXT, $2=CWD, $3=PASSWORD
 
-hash=$(echo -n find $2 -mindepth 1 -type f -print0 | sort -z | sha1sum | sed 's/ .$//' | xargs)
+hash=$(echo -n find "$2" -mindepth 1 -type f -print0 | sort -z | sha1sum | sed 's/ .$//' | xargs)
 if [ $1 == "ENCRYPT" ]; then
-    (cd $2 && zip -q -o -e -r "/tmp/${hash}.zip" ./ -P "$3")
+    (cd "$2" && zip -q -o -e -r "/tmp/${hash}.zip" ./ -P "$3")
     echo "${hash}.zip"
 elif [ $1 == "PLAINTEXT" ]; then
     rm "/tmp/${hash}.zip"
