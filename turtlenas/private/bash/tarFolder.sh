99d517f1206b3ef588a8cc7f5fe11f8dc6cdd227
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-10 15:39:16 -0500

    Update tarFolder.sh

--- turtlenas/private/bash/tarFolder.sh
+++ turtlenas/private/bash/tarFolder.sh
@@ -2,14 +2,18 @@
 
 # $1=ENCRYPT/PLAINTEXT, $2=CWD, $3=PASSWORD
 
-base_name=$(basename $2)
-hash=$(echo -n $base_name | sha224sum | sed 's/  .$//')
-if [[ $1 == "ENCRYPT" ]]; then
-    tar -c -z -f "/tmp/${hash}.tar.gz" $2
-    gpg --batch --passphrase $3 --output "/tmp/${hash}.gpg" -c "/tmp/${hash}.tar.gz"
+base=$(basename "$2")
+hash=$(echo -n find "$2" -mindepth 1 -type f -print0 | sort -z | sha1sum | sed 's/ .$//' | xargs)
+if [ "$1" == "ENCRYPT" ]; then
+    rm "/tmp/${hash}.tar.gz.gpg"
+    (tar -c -z -f "/tmp/${hash}.tar.gz" -C "$2" ".")
+    (gpg --batch --no-options --passphrase "$3" --output "/tmp/${hash}.tar.gz.gpg" --symmetric "/tmp/${hash}.tar.gz")
+    rm -rfi $GNUPGHOME
+    echo "${hash}.tar.gz.gpg"
+elif [ "$1" == "PLAINTEXT" ]; then
     rm "/tmp/${hash}.tar.gz"
-    echo "/tmp/${hash}.gpg"
-elif [[ $1 == "PLAINTEXT" ]]; then
-    tar -c -z -f "/tmp/${hash}.tar.gz" $2
-    echo "/tmp/${hash}.tar.gz"
+    (tar -c -z -f "/tmp/${hash}.tar.gz" -C "$2" ".")
+    echo "${hash}.tar.gz"
 fi
+
+exit
