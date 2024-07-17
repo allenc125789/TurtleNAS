1fa84c38cbd8b526a861d91007f815b5ef231f0b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-16 22:26:12 -0500

    Create create-user.sh

---
+++ create-user.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+
+sWARNING=" ((\033[1;33mWARNING\033[0m)) "
+sERROR=" ((\033[0;31mERROR\033[0m)) "
+vDOMAIN=$(grep "domain" /etc/resolv.conf | awk '{print $NF}')
+vPWD=$(dirname $0)
+vFILESYSTEM=$(df -P . | sed -n '$s/[[:blank:]].*//p')
+vUUID=$(/usr/sbin/blkid -s UUID -o value "$vFILESYSTEM")
+
+
+
+#: Creating Users.
+    #: System Admin.
+sudo useradd -M $1
+    #: Admin.
+if sudo useradd -M $1; then
+    passwd $1
+else
+    :
+fi
+
+
+
+#: SQL.
+    #: Create DB tables.
+mariadb -e "USE turtlenas; CREATE TABLE files_$1 (fullpath NVARCHAR(255) PRIMARY KEY, parent NVARCHAR(255), name NVARCHAR(255),date VARCHAR(224), size VARCHAR(255), mtime CHAR(244) );"
+mariadb -e "USE turtlenas; INSERT INTO drives (user, type, disk, uuid) VALUES('$1', 'LOCAL', '$vFILESYSTEM', '$vUUID');"
+    #: Create DB Users.
+mariadb -e "GRANT ALL PRIVILEGES ON turtlenas.files_$1 TO 'www-data'@'localhost' WITH GRANT OPTION"
