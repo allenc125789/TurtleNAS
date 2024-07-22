e45cf6f5b1b03a7f7e53c042a43b62a3e30522be
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-07-21 23:31:51 -0500

    Update verify.php

--- turtlenas/public/verify.php
+++ turtlenas/public/verify.php
@@ -9,7 +9,7 @@ $password = $_POST['pword'];
 $username = $_POST['uname'];
 
 //$control->user_auth($username, $password);
-echo "<a href='/login.html'>Verifying...</a>";
+echo "<a href='/login.html'>Page not loaded...</a>";
 
 $control->user_auth($username, $password);
 $verify = $control->validate_auth();
