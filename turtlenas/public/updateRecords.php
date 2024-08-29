d38b1ee918ee47d334536f35f7386391b39f56ac
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:16:15 -0500

    Update updateRecords.php

--- turtlenas/public/updateRecords.php
+++ turtlenas/public/updateRecords.php
@@ -2,6 +2,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Update user's SQL file databases. 
 $verify = $control->validate_auth();
 if($verify){
     $control->updateFileRecord();
