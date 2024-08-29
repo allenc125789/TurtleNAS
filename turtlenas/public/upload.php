02c39697f9df3738121558cc700bd661a37c66e5
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:16:37 -0500

    Update upload.php

--- turtlenas/public/upload.php
+++ turtlenas/public/upload.php
@@ -2,6 +2,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Upload file(s) to server.
 $verify = $control->validate_auth();
 if($verify){
     $control->uploadFile();
