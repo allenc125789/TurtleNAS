b0577068c403821c5eae398ab41078b00d442a9b
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:17:00 -0500

    Update uploadDir.php

--- turtlenas/public/uploadDir.php
+++ turtlenas/public/uploadDir.php
@@ -2,6 +2,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Upload a folder to the server.
 $verify = $control->validate_auth();
 if($verify){
     $control->uploadDir();
