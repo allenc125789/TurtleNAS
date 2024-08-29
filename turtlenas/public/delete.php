75a9816ed3a087624532d5b3a146cba99af91d04
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:08:12 -0500

    Update delete.php

--- turtlenas/public/delete.php
+++ turtlenas/public/delete.php
@@ -3,7 +3,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
-#Calls delete function on verification.
+#Calls the Delete function on verification.
 $verify = $control->validate_auth();
 if($verify){
     $control->deleteFile();
