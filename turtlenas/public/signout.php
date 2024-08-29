6a73eb0dbf0f849002789e88d40bdadb96800d48
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:14:17 -0500

    Update signout.php

--- turtlenas/public/signout.php
+++ turtlenas/public/signout.php
@@ -2,6 +2,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Sign out of account (ends php session).
 $verify = $control->validate_auth();
 if ($verify){
     $fObject = $control->signout();
