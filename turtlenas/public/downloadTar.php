c49aa4dcb6023cdfb331e4fbe289c672b39c079a
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:12:28 -0500

    Update downloadTar.php

--- turtlenas/public/downloadTar.php
+++ turtlenas/public/downloadTar.php
@@ -3,6 +3,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Download a tar file of a folder. Encryption depending on query as args.
 $verify = $control->validate_auth();
 if($verify){
     if ($query = urldecode($_SERVER['QUERY_STRING']) === "PLAINTEXT"){
