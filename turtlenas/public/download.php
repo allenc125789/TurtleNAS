6ecb3ec3ad9b8a4b8738b157a1b457ba815d3a8e
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-29 03:08:00 -0500

    Update download.php

--- turtlenas/public/download.php
+++ turtlenas/public/download.php
@@ -3,6 +3,7 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
+#Calls Download function on verification.
 $verify = $control->validate_auth();
 if($verify){
     $control->getDownload();
