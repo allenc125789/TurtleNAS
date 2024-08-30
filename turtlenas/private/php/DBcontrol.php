c91399d463e4bc612ae4b504199a52fb660ca139
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-08-30 18:20:54 -0500

    Update DBcontrol.php

--- turtlenas/private/php/DBcontrol.php
+++ turtlenas/private/php/DBcontrol.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
 
-error_reporting(-1); // display all faires
-ini_set('display_errors', 1);  // ensure that faires will be seen
-ini_set('display_startup_errors', 1); // display faires that didn't born
-
-
-
 class DBcontrol {
     private $host;
     private $dbname;
