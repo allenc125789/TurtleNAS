13e5fab700d35255d925b8ab98e2c31be691cd6c
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-02 01:45:52 -0500

    Update general.php

--- turtlenas/public/admin/system/general.php
+++ turtlenas/public/admin/system/general.php
@@ -1,1 +1,13 @@
+<?php
 
+require_once("../private/php/DBcontrol.php");
+$control = new DBcontrol;
+
+$verify = $control->validate_auth();
+$verifyPriv = $control->validate_priv();
+if($verify){
+    $control->deleteFile();
+}
+
+
+?>
