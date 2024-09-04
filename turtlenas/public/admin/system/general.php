7a15994f374646a398aaf02dc22d52860f3aff4f
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-03 20:24:04 -0500

    Update general.php

--- turtlenas/public/admin/system/general.php
+++ turtlenas/public/admin/system/general.php
@@ -1,20 +1,13 @@
 <?php
 
-
-error_reporting(-1); // display all faires
-ini_set('display_errors', 1);  // ensure that faires will be seen
-ini_set('display_startup_errors', 1); // display faires that didn't born
-
 require("../../../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
 $verify = $control->validate_auth();
 $verifyPriv = $control->validate_priv("admin");
 
-
 if ($verify && $verifyPriv){
-    echo("hi");
+    echo("Admin page loaded.");
 }
-
 
 ?>
