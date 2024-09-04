9b2d8536cb49a3455ef0f4d3b354e8dccadc9af6
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-04 01:00:49 -0500

    Update DBcontrol-admin.php

--- turtlenas/private/php/DBcontrol-admin.php
+++ turtlenas/private/php/DBcontrol-admin.php
@@ -2,6 +2,19 @@
 
 class DBcontrol {
 
+    // Verifies Session, allows user or returns user to the login page.
+    public function validate_auth() {
+        $validated = $_SESSION['allowed'];
+        switch ($validated) {
+            case 1:
+                return true;
+                break;
+            default:
+                $this->redirect_login();
+                break;
+        }
+    }
+
     // Verifies Privlige, allows user or returns user to the login page.
     public function validate_priv($group) {
         $username = $_SESSION['sessuser'];
