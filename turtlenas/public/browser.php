56a5bb45f6fa5d464884f77e368af8961063b173
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-03 20:22:58 -0500

    Update browser.php

--- turtlenas/public/browser.php
+++ turtlenas/public/browser.php
@@ -139,6 +139,10 @@ if ($verify){
     <!--Button to sign out of account.-->
     <label for="signOut" id="signOutTxt" class="buttonTxt">Log-out</label>
     <button class="buttons" id="signOut" onclick="getRequestSignOut()"></button>
+
+    <!--Button to switch to the admin view.-->
+    <label for="adminView" id="adminViewTxt" class="buttonTxt">Admin View</label>
+    <button class="buttons" id="adminView" onclick="getRequestAdminView()"></button>
 </div>
 
 <!--Screen blocking div for when a request is "loading".-->
@@ -791,4 +795,3 @@ document.getElementById("logOutput").insertAdjacentHTML('beforeEnd', logcookie);
         };
 </script>
 </html>
-
