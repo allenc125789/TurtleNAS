cdaba546eee13d9cecc743dd7636b30c9457e4c1
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-03 23:04:47 -0500

    Create DBcontrol-admin.php

---
+++ turtlenas/private/php/DBcontrol-admin.php
@@ -0,0 +1,18 @@
+class DBcontrol {
+
+    // Verifies Privlige, allows user or returns user to the login page.
+    public function validate_priv($group) {
+        $username = $_SESSION['sessuser'];
+        $command = shell_exec(' bash ../../../private/bash/validate-group.sh '.escapeshellarg($username)." ".escapeshellarg($group));
+        $output = var_dump($command);
+        switch ($command) {
+            case 1:
+                return true;
+                break;
+            default:
+                $this->redirect_login();
+                break;
+        }
+    }
+
+}
