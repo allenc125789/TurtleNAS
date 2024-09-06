e462e9d0d8e52b07fac2969de3c5ed8b96447705
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-05 19:46:57 -0500

    Create admin-general.php

---
+++ turtlenas/private/php/admin-general.php
@@ -0,0 +1,22 @@
+<div class='pageContents'>
+    <text>Logged in as:</text>
+    <br><br>
+    <text>Uptime:</text>
+    <br><br>
+    <text>Computer Name:</text>
+    <br>
+    <text>Domain Name:</text>
+    <br>
+    <text>IP Address:</text>
+    <br><br>
+
+    <text>CPU(s):</text>
+    <br>
+    <text>RAM:</text>
+    <br><br>
+
+    <text>Debian version:</text>
+    <br>
+    <text>TurtleNAS version:</text>
+    <br>
+</div>
