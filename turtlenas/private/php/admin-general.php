b63c95a89a01665c0e5d6ffdc1b4ce0b03903af0
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-05 21:38:19 -0500

    Update admin-general.php

--- turtlenas/private/php/admin-general.php
+++ turtlenas/private/php/admin-general.php
@@ -1,16 +1,42 @@
+<?php
+
+function printUptime(){
+    $uptime = shell_exec(' uptime -p');
+    $uptime = ltrim($uptime, "up ");
+    return $uptime;
+}
+
+function execPrintCPUs(){
+    $grep = shell_exec(" grep 'cpu cores' /proc/cpuinfo | uniq");
+    $grep = ltrim($grep, "cpu cores\t:");
+    return $grep;
+}
+
+function execPrint($command){
+    $uptime = shell_exec(" $command");
+    return $uptime;
+}
+
+$printUptime = printUptime();
+$printHostname = execPrint("hostname");
+$printDomainname = execPrint("domainname");
+$printIP = execPrint("hostname -I");
+$printCPUs = execPrintCPUs();
+?>
+
 <div class='pageContents'>
-    <text>Logged in as:</text>
+    <text>Logged in as: <?php echo($_SESSION['sessuser']);?></text>
     <br><br>
-    <text>Uptime:</text>
+    <text>Uptime: <?php echo($printUptime);?></text>
     <br><br>
-    <text>Computer Name:</text>
+    <text>Computer Name: <?php echo($printHostname);?></text>
     <br>
-    <text>Domain Name:</text>
+    <text>Domain Name: <?php echo($printDomainname);?></text>
     <br>
-    <text>IP Address:</text>
+    <text>IP Address: <?php echo($printIP);?></text>
     <br><br>
 
-    <text>CPU(s):</text>
+    <text>CPU(s): <?php echo($printCPUs);?></text>
     <br>
     <text>RAM:</text>
     <br><br>
