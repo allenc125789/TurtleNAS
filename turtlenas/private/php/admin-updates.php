dcb773fda9492541a664ae4f6e34be8abff28d81
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-10 18:30:55 -0500

    Update admin-updates.php

--- turtlenas/private/php/admin-updates.php
+++ turtlenas/private/php/admin-updates.php
@@ -4,12 +4,14 @@
 ?>
 
 <!--Account Management section.-->
-<div class='pageContents' id='updateMenuDiv'>
-    <!--Button to sign out of account.-->
-    <label for="upgrade" id="upgradeTxt" class="buttonTxt">Upgrade</label>
-    <button class="buttons" id="upgrade" onclick="#"></button>
+<div class='pageContents'>
+    <div id='updateMenuDiv'>
+        <!--Button to switch to the browser view.-->
+        <label for="refresh" id="refreshTxt" class="buttonTxt">Refresh</label>
+        <input class="buttons" id="refresh" type="#">
 
-    <!--Button to switch to the browser view.-->
-    <label for="refresh" id="refreshTxt" class="buttonTxt">Refresh</label>
-    <input class="buttons" id="refresh" type="#">
+        <!--Button to sign out of account.-->
+        <label for="upgrade" id="upgradeTxt" class="buttonTxt">Upgrade</label>
+        <button class="buttons" id="upgrade" onclick="#"></button>
+    </div>
 </div>
