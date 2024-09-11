0768e4c52959808b41c7313da94a09b784d54749
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-11 12:32:16 -0500

    Update admin-updates.php

--- turtlenas/private/php/admin-updates.php
+++ turtlenas/private/php/admin-updates.php
@@ -8,7 +8,7 @@ $printUpdatesCount = $control->printUpdateList(TRUE);
     <div id='updateMenuDiv'>
         <!--Button to switch to the browser view.-->
         <label for="refresh" id="refreshTxt" class="buttonTxt">Refresh</label>
-        <input class="buttons" id="refresh" type="#">
+        <input class="buttons" id="refresh" onclick="getRequestAptUpdate()">
 
         <!--Button to sign out of account.-->
         <label for="upgrade" id="upgradeTxt" class="buttonTxt">Upgrade</label>
@@ -21,3 +21,21 @@ $printUpdatesCount = $control->printUpdateList(TRUE);
     </div>
 
 </div>
+
+<script type='text/javascript'>
+//Request to sign out of account, and sends user back to the login page.
+function getRequestAptUpdate(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    // Write code for writing output when databse updates start.:
+        if (this.readyState == 4 && this.status == 200){
+           // Write code for writing output when databse is fully updated.:
+            location.reload();
+        } else if(this.status >= 400){
+            location.reload();
+        }
+    };
+    xhttp.open("GET", "/admin/system/requestAptUpdate.php", true);
+    xhttp.send();
+}
+</script>
