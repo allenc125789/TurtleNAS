677c1cc1b06df4ab31c668317b9da2b2961f8892
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-11 11:38:38 -0500

    Create requestAptUpdate.php

---
+++ turtlenas/private/php/requestAptUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+require("../../../private/php/DBcontrol-admin.php");
+$control = new DBcontrol;
+
+$verify = $control->validate_auth();
+$verifyPriv = $control->validate_priv("admin");
+
+if ($verify && $verifyPriv){
+    $control->requestAptUpdate();
+}
+
+?>
