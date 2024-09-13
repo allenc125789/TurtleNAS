0f328feb335fb967781bcb27084f73d64a424e61
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-12 20:53:00 -0500

    Update requestAptUpgrade.php

--- turtlenas/public/admin/system/requestAptUpgrade.php
+++ turtlenas/public/admin/system/requestAptUpgrade.php
@@ -1,1 +1,13 @@
+<?php
 
+require("../../../private/php/admin-DBcontrol.php");
+$control = new DBcontrol;
+
+$verify = $control->validate_auth();
+$verifyPriv = $control->validate_priv("admin");
+
+if ($verify && $verifyPriv){
+    $control->requestAptUpdate();
+}
+
+?>
