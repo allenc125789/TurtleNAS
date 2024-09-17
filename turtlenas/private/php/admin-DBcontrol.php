88d161ab5deab973a1b620dd690310e2a501198e
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-09-16 23:38:33 -0500

    Update admin-DBcontrol.php

--- turtlenas/private/php/admin-DBcontrol.php
+++ turtlenas/private/php/admin-DBcontrol.php
@@ -36,12 +36,16 @@ class DBcontrol {
     }
 
     public function printUpdateList($count = FALSE) {
+        ob_start();
         if ($count == FALSE){
             $command = shell_exec("apt-get --just-print dist-upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print \"+<b>$1</b> [CURRENT: $2, NEW: $3]<br>\"}'");
+            ob_flush();
         } else {
             $command = shell_exec('apt list --upgradable 2>/dev/null | tail -n+2 | wc -l | tr -d "[:space:]"');
+            ob_flush();
         }
         return $command;
+        ob_end_clean();
     }
 
     public function requestAptUpdate() {
@@ -51,7 +55,6 @@ class DBcontrol {
     public function requestAptUpgrade() {
         shell_exec("sudo apt-get -y upgrade");
     }
-
 }
 
 ?>
