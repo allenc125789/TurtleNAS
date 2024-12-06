eb142e6f9f454e0c4aca5685e9e4c77b7c0a65ed
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-06 00:34:30 -0600

    Update admin-DBcontrol.php

--- turtlenas/private/php/admin-DBcontrol.php
+++ turtlenas/private/php/admin-DBcontrol.php
@@ -21,7 +21,7 @@ class DBcontrol {
         }
     }
 
-    // Verifies Privlige, allows user or returns user to the login page.
+    // Verifies Privlige for group argument, allows user or returns user to the login page.
     public function validate_priv($group) {
         $username = $_SESSION['sessuser'];
         $command = shell_exec(' bash ../../../private/bash/validate-group.sh '.escapeshellarg($username)." ".escapeshellarg($group));
