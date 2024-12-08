a0b71218383d3972b2acddd2f7d0c689ec3bc1f6
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-08 15:18:33 -0600

    Update admin-interfaces.php

--- turtlenas/private/php/admin-interfaces.php
+++ turtlenas/private/php/admin-interfaces.php
@@ -47,22 +47,6 @@ function windowBlockON(){
     document.getElementById("window-block").style.visibility = "visible";
 }
 
-function getRequestAptUpdate(){
-    var xhttp = new XMLHttpRequest();
-    windowBlockON();
-    xhttp.onreadystatechange = function(){
-    // Write code for writing output when databse updates start.:
-        if (this.readyState == 4 && this.status == 200){
-           // Write code for writing output when databse is fully updated.:
-            location.reload();
-        } else if(this.status >= 400){
-            location.reload();
-        }
-    };
-    xhttp.open("GET", "/admin/system/requestAptUpdate.php", true);
-    xhttp.send();
-}
-
 function openConsole(){
     var console = document.getElementById("console");
     windowBlockON();
