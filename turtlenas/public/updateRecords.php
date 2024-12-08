1a8f36168a11257873b2cffb7b01898df595f9a3
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-07 21:51:36 -0600

    Update updateRecords.php

--- turtlenas/public/updateRecords.php
+++ turtlenas/public/updateRecords.php
@@ -2,8 +2,12 @@
 require_once("../private/php/DBcontrol.php");
 $control = new DBcontrol;
 
-$verify = $control->validate_auth();
-if($verify){
+//Verfies creds.
+$groups = "www-data";
+$auth = $control->validate_auth();
+$priv = $control->validate_priv($groups);
+
+if($auth && $priv){
     $control->updateFileRecord();
 }
 
