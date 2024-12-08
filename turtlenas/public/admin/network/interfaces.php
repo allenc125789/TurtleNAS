56861b917fca7eebd8137732a585c01ddc1f2d86
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-08 14:07:49 -0600

    Update interfaces.php

--- turtlenas/public/admin/network/interfaces.php
+++ turtlenas/public/admin/network/interfaces.php
@@ -1,1 +1,15 @@
+<?php
 
+require("../../../private/php/admin-DBcontrol.php");
+$control = new DBcontrol;
+
+//Verfies creds.
+$groups = "admin";
+$auth = $control->validate_auth();
+$priv = $control->validate_priv($groups);
+
+if($auth && $priv){
+    include("../../../private/php/admin-interfaces.php");
+}
+
+?>
