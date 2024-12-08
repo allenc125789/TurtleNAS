e36babff9de4dc0ea7049c23931a86279e7c27dc
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-07 21:57:41 -0600

    Update general.php

--- turtlenas/public/admin/system/general.php
+++ turtlenas/public/admin/system/general.php
@@ -3,10 +3,12 @@
 require("../../../private/php/admin-DBcontrol.php");
 $control = new DBcontrol;
 
-$verify = $control->validate_auth();
-$verifyPriv = $control->validate_priv("admin");
+//Verfies creds.
+$groups = "www-data";
+$auth = $control->validate_auth();
+$priv = $control->validate_priv($groups);
 
-if ($verify && $verifyPriv){
+if($auth && $priv){
     include("../../../private/php/admin-general.php");
 }
 
