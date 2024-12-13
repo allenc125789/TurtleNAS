f5513407b3e5cabb8428dce02480fc912b6c3ed5
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-13 05:11:06 -0600

    Update requestInterfaceStatusChange.php

--- turtlenas/public/admin/network/requestInterfaceStatusChange.php
+++ turtlenas/public/admin/network/requestInterfaceStatusChange.php
@@ -9,7 +9,7 @@ $auth = $control->validate_auth();
 $priv = $control->validate_priv($groups);
 
 if($auth && $priv){
-    $control->changeInterfaceStatus();
+    $control->changeInterfaceStatus($interface, $status);
 }
 
 ?>
