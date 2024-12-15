401f3098253b9812fa35161be863cca93255e81c
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-15 02:52:33 -0600

    Update admin-DBcontrol.php

--- turtlenas/private/php/admin-DBcontrol.php
+++ turtlenas/private/php/admin-DBcontrol.php
@@ -71,7 +71,7 @@ class DBcontrol {
         $n = array_pop($names);
 
         foreach ($names as $i){
-            $command = shell_exec("sudo ifdown $i && sudo ifup $i");
+            $command = shell_exec("sudo dhclient -r $i && sudo dhclient $i");
         }
     }
 
