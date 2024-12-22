d64300010fc101de414904f5aa531c2ade5424a7
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-21 20:39:40 -0600

    Create admin-datetime.php

---
+++ turtlenas/private/php/admin-datetime.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<html>
+
+<head>
+    <meta charset="UTF=8">
+    <meta name="viewport" content="width==device-width, initial-scale=0.1">
+    <link rel="stylesheet" href="/css/admin.css">
+</head>
+
+<?php include("../../../private/html/admin-pageSelectMenu.html");?>
+
+<div class='pageContents'>
+    <div class='pageTitle'>
+        <h1>Date/Time</h1>
+        <hr>
+        <br>
+    </div>
+</div>
+</html>
