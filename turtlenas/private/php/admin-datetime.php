03d60801efd5370a05991cf9597f64ebeceacc3e
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-12-23 20:10:42 -0600

    Update admin-datetime.php

--- turtlenas/private/php/admin-datetime.php
+++ turtlenas/private/php/admin-datetime.php
@@ -1,5 +1,14 @@
 <?php
 
+function execPrintTimezones(){
+    $command = shell_exec("timedatectl list-timezones | more | cat | sed 's/$/\|/' | tr -d '[:space:]'");
+    $timezones = explode("|", $command);
+    $n = array_pop($timezones);
+    return $timezones;
+}
+
+$displayTimezones = execPrintTimezones();
+
 ?>
 
 <html>
@@ -17,23 +26,61 @@
         <hr>
         <br>
     </div>
-    <text><b>Time Zone: </b></text>
-        <!--Dropdown button for network.-->
-    <div id='timezone-menu'>
-      <label for="interface-btn" id="signOutTxt" class="dropdownTxt">Network</label>
-      <button id="interface-btn" class="dropdown-btn">Network
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container">
-        <a class="dropdownItem" href="/admin/network/interfaces.php">Interfaces</a><br>
-        <a class="dropdownItem" href="#">Connections</a><br>
-        <a class="dropdownItem" href="#">Limits</a><br>
-      </div>
-    </div>
+        <text><b>Time Zone: </b></text>
+        <!--Dropdown button for timezones.-->
+        <div id='timezone-menu'>
+            <label for="interface-btn" id="timeout-current" class="dropdownTxt"><?php echo(shell_exec("timedatectl | awk '/Time zone: / {print $3}'")); ?></label>
+            <button id="interface-btn" class="dropdown-btn">
+            <i class="fa fa-caret-down"></i>
+            </button>
+            <div id="container" class="dropdown-container">
+            </div>
+         </div>
     <br>
     <br>
     <text><b>Server Time: </b><?php echo(shell_exec('date')); ?></text>
 </div>
 
 <?php include("../../../private/html/admin-pageSelectMenu.html");?>
+
+<script type='text/javascript'>
+//Refreshes the file-table and organizes files for display when called.
+function displayTimezones(){
+    var container = document.getElementById("container");
+    //If files are found, start processing them into the table.
+    //Establishes table content variables.
+    for (i in jArray){
+        container.insertAdjacentHTML('beforeEnd', "<button class='dropdownItem'> "+jArray[i]+".</button>");
+/*
+        if (jArray[i]['name'] == 'lo'){
+            checkboxes.setAttribute("disabled", "disabled");
+            checkboxes.setAttribute("checked", "true");
+        }
+        if (jArray[i]['status'] == 'UP'){
+            checkboxes.setAttribute("checked", "true");
+        }
+        //Sets cell 0 as a checkbox.
+        cell0.appendChild(checkboxes);
+        cell0.setAttribute("class", "checks");
+        //Sets cell content.
+        cell0.insertAdjacentHTML('beforeEnd', "<text> "+jArray[i]['number']+".</text>");
+        cell1.insertAdjacentHTML('beforeEnd', "<text>"+jArray[i]['name']+"</text>");
+        cell2.insertAdjacentHTML('beforeEnd', "<text>"+jArray[i]['status']+"</text>");
+        cell3.insertAdjacentHTML('beforeEnd', "<text>"+jArray[i]['ip']+"</text>");
+        //Sets netmask cell.
+        if (jArray[i]['netmask'] == "EMPTY"){
+            cell4.insertAdjacentHTML('beforeEnd', "<text>"+jArray[i]['netmask']+"</text>");
+        }else {
+            cell4.insertAdjacentHTML('beforeEnd', "<text>/"+jArray[i]['netmask']+"</text>");
+        }
+        cell5.insertAdjacentHTML('beforeEnd', "<text>"+jArray[i]['gateway']+"</text>");
+*/    }
+}
+
+
+
+let jArray = <?php echo json_encode($displayTimezones); ?>;
+displayTimezones();
+
+</script>
 </html>
